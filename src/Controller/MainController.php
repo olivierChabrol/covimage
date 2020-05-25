448da35b4502e5672bf743d2ca3bb4d8254ddd57
Manvan33 <rikostik@gmail.com>
2020-05-25 17:00:41 +0200

    Lien avec la BDD, VichUploaderBundle

---
+++ src/Controller/MainController.php
@@ -0,0 +1,25 @@
+<?php
+// src/Controller/Main.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('home.html.twig');
+    }
+    public function upload()
+    {
+        return $this->render('upload.html.twig');
+    }
+    public function files(Request $req)
+    {
+        $requestStr = var_dump($req);
+        return new Response("Uploadé");
+    }
+}
+?>
---
+++ src/Entity/Fichier.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FichierRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+/**
+ * @ORM\Entity(repositoryClass=FichierRepository::class)
+ * @Vich\Uploadable
+ */
+class Fichier
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * NOTE: This is not a mapped field of entity metadata, just a simple property.
+     * 
+     * @Vich\UploadableField(mapping="uploaded_files", fileNameProperty="nom", size="fileSize")
+     * 
+     * @var File|null
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="blob", nullable=true)
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu($contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
