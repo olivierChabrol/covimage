a819d45ded9b637f4960deba6d4d4952df05b699
Astrid <astrid.beyer@etu.univ-amu.fr>
2020-05-25 14:53:46 +0200

    navbar bootstrap

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,17 +1,26 @@
 <?php
 // src/Controller/LuckyController.php
+
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
+    public function __construct(Environment $twig)
+    {
+        $loader = $twig->getLoader();
+    }
+
     public function number()
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+
     }
 }
