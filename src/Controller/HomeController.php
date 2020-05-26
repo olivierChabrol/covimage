2689277d680d8a13a076e776afac89d93e50b5e0
Astrid <astrid.beyer@etu.univ-amu.fr>
2020-05-26 10:22:01 +0200

    homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+// src/Controller/HomeController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
+
+class HomeController extends AbstractController
+{
+    public function homepage()
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            $username = "inconnu";
+        } else {
+            $username = $user->getFirstName();
+        }
+        
+        return $this->render('home/username.html.twig', [
+            'username' => $username,
+        ]);
+    }
+}
