bcda3afea9864029756b5b522b0ef8830947bd64
Astrid <astrid.beyer@etu.univ-amu.fr>
2020-05-28 15:30:30 +0200

    onglet /admin

---
+++ src/Form/UserType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'Maybe' => "",
+                    'USER' => "ROLE_USER",
+                    'ADMIN' => "ROLE_USER",
+                ]])
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
