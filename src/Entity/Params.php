4ee4ef81a86e44c2e307deaf3fd34c812514f9a0
Lucas <lurgenti@gmail.com>
2020-06-02 14:15:50 +0200

    Params

---
+++ src/Entity/Params.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ParamsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ParamsRepository::class)
+ */
+class Params
+{
+    public const PARAM = 1;
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $type_param;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $value;
+
+    /**
+     * @ORM\Column(type="string", length=8, nullable=true)
+     */
+    private $color;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTypeParam(): ?int
+    {
+        return $this->type_param;
+    }
+
+    public function setTypeParam(int $type_param): self
+    {
+        $this->type_param = $type_param;
+
+        return $this;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+}
